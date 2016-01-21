fe9f25ebb27defa752f29428da2116305a7f9bf3
id101010 <aaron@duckpond.ch>
2016-01-21 18:35:36 +0100

    serparlatch bugfix

--- serparlatch.vhd
+++ serparlatch.vhd
@@ -45,17 +45,19 @@ begin
     -- outputs : parallel_out
     process(clk)
     begin
-        if (rising_edge(clear)) then                                            -- if a clear signal gets detected, clear everything                        
-            parallel_output_latch <= (others => '0');
-            parallel_output <= (others => '0');
-        end if;
-        if (rising_edge(shift_down)) then                                       -- shift down all bits and read a new one into the latch
-            parallel_output_latch(nbits downto 1) <= parallel_output_latch((nbits - 1) downto 0);
-            parallel_output_latch(0) <= serial_in;
-        end if;
-        if (rising_edge(shift_out)) then                                        -- make the contents of the latch visible
-            parallel_output(nbits downto 0) <= parallel_output_latch(nbits downto 0);
-            parallel_output_latch <= (others => '0');
+        if(rising_edge(clk)) then
+            if (clear = '1') then                                            -- if a clear signal gets detected, clear everything                        
+                parallel_output_latch <= (others => '0');
+                parallel_output <= (others => '0');
+            end if;
+            if (shift_down = '1') then                                       -- shift down all bits and read a new one into the latch
+                parallel_output_latch(nbits downto 1) <= parallel_output_latch((nbits - 1) downto 0);
+                parallel_output_latch(0) <= serial_in;
+            end if;
+            if (shift_out = '1') then                                        -- make the contents of the latch visible
+                parallel_output(nbits downto 0) <= parallel_output_latch(nbits downto 0);
+                parallel_output_latch <= (others => '0');
+            end if;
         end if;
     end process;
 end Behavioral;
